fd418c59b34b9add1eabc35dc2493000891c9111
chea-young <63410509+chea-young@users.noreply.github.com>
2025-10-28 20:53:22 +0900

    Time: 19 ms (7.09%), Space: 45.1 MB (75.78%) - LeetHub

---
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.java
@@ -0,0 +1,63 @@
+/**
+
+bank: 2진수 배열 
+0: empty
+1: 보안 장비
+
+2개 보안 장비 사이에 1개의 레이저
+- 
+
+결론
+- 총 레이저 수
+ */
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int m = bank[0].length();
+        int n = bank.length;
+
+        if (n == 1) {
+            return 0;
+        }
+
+        int answer = 0;
+    
+        for (int i=0; i< n-1; i++) {
+            int nextSecurityDeviceCnt = 0;  
+
+            // 보안장비가 있는 열 찾기
+            int pluxRow = 0;
+            for (int row=i; row<n-1; row++) {
+                nextSecurityDeviceCnt = countSecurityDevices(bank[row+1]);
+                if (nextSecurityDeviceCnt != 0) {
+                    break;
+                }
+
+                pluxRow++;
+            }
+            
+            // 레이저 수 더하기
+            for (int j=0; j < m; j++) {
+                if (bank[i].charAt(j) == '1') {
+                    answer += nextSecurityDeviceCnt;
+                }
+            }
+
+            // 그 다음 보안 장비가 있는 열도 이동시키기
+            i += pluxRow;
+        }
+
+        return answer;
+    }
+
+    private int countSecurityDevices(String row) {
+        int result = 0;
+
+        for (int i=0; i<row.length(); i++) {
+            if (row.charAt(i) == '1') {
+                result += 1;
+            }
+        }
+
+        return result;
+    }
+}
